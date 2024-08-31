a2b12398acc1c3b426e7c02c5ab4c7e800ac983a
canguldev <canguldev@gmail.com>
2024-08-31 13:43:00 +0300

    App created

---
+++ MyNews/Scenes/Home Page/Views/HomePageVC.swift
@@ -0,0 +1,16 @@
+//
+//  HomePageVC.swift
+//  MyNews
+//
+//  Created by Can Gül on 31.08.2024.
+//
+
+import UIKit
+
+class HomePageVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
